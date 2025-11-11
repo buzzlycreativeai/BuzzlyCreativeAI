dfc08525a7d87b4c2b7d032ca05b89df3b5ab220
buzzlycreativeai <buzzlycreative.ai@gmail.com>
2025-11-11 08:34:50 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,2 +1,14 @@
 const nextConfig = { reactStrictMode: true };
 module.exports = nextConfig;
+
+New Chat
+10 lines
+
+  import { SessionProvider } from 'next-auth/react';
+  export default function App({ Component, pageProps: { session, ...pageProps } }) {
+    return (
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    );
+  }
